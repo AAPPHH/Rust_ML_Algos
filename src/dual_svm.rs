ca462cfddd948b4c474dd742423e38c3d26a56bb
AAPPHH <jfhamelmann@gmail.com>
2025-06-14 18:05:13 +0200

    cache faer

--- src/dual_svm.rs
+++ src/dual_svm.rs
@@ -2,15 +2,13 @@ use crate::flat_dataset::FlatDataset;
 use crate::flat_kernel_cache::FlatKernelCache;
 use crate::svm_kernel::KernelType;
 use crate::working_set::select_working_set_wss2_flat_cache;
-use faer::{Mat, prelude::*};
-
-
+use faer::Mat;
 
 #[derive(Clone)]
 pub struct DualSVM {
-    pub alphas: Option<Mat<f64>>,             // (n_sv, 1)
-    pub support_vectors: Option<FlatDataset>, // FlatDataset nutzt bereits Mat<f64>
-    pub support_labels: Option<Mat<f64>>,     // (n_sv, 1)
+    pub alphas: Option<Mat<f64>>,
+    pub support_vectors: Option<FlatDataset>,
+    pub support_labels: Option<Mat<f64>>,
     pub bias: f64,
     pub c: f64,
     pub kernel: KernelType,
@@ -28,13 +26,7 @@ impl DualSVM {
         }
     }
 
-    pub fn fit(
-        &mut self,
-        dataset: FlatDataset,
-        y: Vec<f64>,
-        max_iter: usize,
-        tol: f64,
-    ) {
+    pub fn fit(&mut self, dataset: FlatDataset, y: Vec<f64>, max_iter: usize, tol: f64) {
         let n = dataset.n_samples();
         let mut alphas = Mat::<f64>::zeros(n, 1);
         let mut bias = 0.0;
@@ -42,23 +34,22 @@ impl DualSVM {
         let mut passes = 0;
         let max_passes = 5;
         let mut iter = 0;
-        let mut active_set: Vec<usize> = (0..n).collect();
-        let mut active_size = n;
         let min_iter = 100;
-
         let mut grad = Mat::<f64>::from_fn(n, 1, |i, _| -y[i]);
         let mut kernel_cache = FlatKernelCache::new(self.kernel.clone(), dataset.clone(), 100);
+        let active_set: Vec<usize> = (0..n).collect();
 
         while (passes < max_passes || iter < min_iter) && iter < max_iter {
             let mut maximal_violation: f64 = 0.0;
 
+
             while let Some(((ii, jj), violation)) = select_working_set_wss2_flat_cache(
-                alphas.col_as_slice(0), // <- benötigt Vec, falls du select_working_set noch nicht auf Mat umgestellt hast
+                alphas.col_as_slice(0),
                 &y,
                 grad.col_as_slice(0),
                 c,
                 &mut kernel_cache,
-                &active_set[..active_size],
+                &active_set,
             ) {
                 let i = active_set[ii];
                 let j = active_set[jj];
@@ -77,7 +68,6 @@ impl DualSVM {
                 let kii = kernel_cache.get(i, i);
                 let kjj = kernel_cache.get(j, j);
                 let kij = kernel_cache.get(i, j);
-
                 let eta = 2.0 * kij - kii - kjj;
                 if eta >= 0.0 { break; }
 
@@ -89,13 +79,9 @@ impl DualSVM {
 
                 let b1 = bias - gi - yi * (ai_new - ai_old) * kii - yj * (aj_new - aj_old) * kij;
                 let b2 = bias - gj - yi * (ai_new - ai_old) * kij - yj * (aj_new - aj_old) * kjj;
-                bias = if ai_new > 0.0 && ai_new < c {
-                    b1
-                } else if aj_new > 0.0 && aj_new < c {
-                    b2
-                } else {
-                    (b1 + b2) / 2.0
-                };
+                bias = if ai_new > 0.0 && ai_new < c { b1 }
+                    else if aj_new > 0.0 && aj_new < c { b2 }
+                    else { (b1 + b2) / 2.0 };
 
                 alphas.write(i, 0, ai_new);
                 alphas.write(j, 0, aj_new);
@@ -103,47 +89,30 @@ impl DualSVM {
                 let delta_ai = ai_new - ai_old;
                 let delta_aj = aj_new - aj_old;
 
-                for &k in &active_set[..active_size] {
+                for &k in &active_set {
                     let ki = kernel_cache.get(i, k);
                     let kj = kernel_cache.get(j, k);
                     let update = ki * yi * delta_ai + kj * yj * delta_aj;
-                    let new_val = grad.read(k, 0) + update;
-                    grad.write(k, 0, new_val);
+                    grad.write(k, 0, grad.read(k, 0) + update);
                 }
             }
 
-            if maximal_violation < tol {
-                passes += 1;
-            } else {
-                passes = 0;
-            }
+            if maximal_violation < tol { passes += 1; } else { passes = 0; }
             iter += 1;
         }
 
-        // Support-Vector-Maske und Indizes
-        let mask: Vec<bool> = (0..n).map(|i| alphas.read(i, 0) > 1e-8).collect();
-        let sv_idx: Vec<usize> = mask
-            .iter()
-            .enumerate()
-            .filter(|&(_, &m)| m)
-            .map(|(i, _)| i)
-            .collect();
+        let sv_idx: Vec<usize> = (0..n).filter(|&i| alphas.read(i, 0) > 1e-8).collect();
 
         let mut sv_mat = Mat::zeros(sv_idx.len(), dataset.n_features());
         for (row_idx, &i) in sv_idx.iter().enumerate() {
             for j in 0..dataset.n_features() {
-                let val = dataset.data.read(i, j);
-                sv_mat.write(row_idx, j, val);
+                sv_mat.write(row_idx, j, dataset.data.read(i, j));
             }
         }
 
-        let sv_dataset = FlatDataset { data: sv_mat };
-        let sv_labels = Mat::from_fn(sv_idx.len(), 1, |i, _| y[sv_idx[i]]);
-        let sv_alphas = Mat::from_fn(sv_idx.len(), 1, |i, _| alphas.read(sv_idx[i], 0));
-
-        self.support_vectors = Some(sv_dataset);
-        self.support_labels = Some(sv_labels);
-        self.alphas = Some(sv_alphas);
+        self.support_vectors = Some(FlatDataset { data: sv_mat });
+        self.support_labels = Some(Mat::from_fn(sv_idx.len(), 1, |i, _| y[sv_idx[i]]));
+        self.alphas = Some(Mat::from_fn(sv_idx.len(), 1, |i, _| alphas.read(sv_idx[i], 0)));
         self.bias = bias;
     }
 
@@ -155,15 +124,10 @@ impl DualSVM {
         let bias = self.bias;
 
         if let KernelType::Linear = self.kernel {
-            let x_mat = dataset.data.as_ref();
-            let sv_mat = sv.data.as_ref();
-            let kernel_matrix = x_mat * sv_mat.transpose();
+            let kernel_matrix = dataset.data.as_ref() * sv.data.as_ref().transpose();
             let result_mat = &kernel_matrix * &coeff;
-            return (0..result_mat.nrows())
-                .map(|i| result_mat.read(i, 0) + bias)
-                .collect();
+            return (0..result_mat.nrows()).map(|i| result_mat.read(i, 0) + bias).collect();
         }
-
 
         let mut result = Vec::with_capacity(dataset.n_samples());
         for i in 0..dataset.n_samples() {
--- src/flat_kernel_cache.rs
+++ src/flat_kernel_cache.rs
@@ -4,29 +4,26 @@ use faer::Mat;
 
 #[derive(Clone)]
 pub struct FlatKernelCache {
-    kernel:  KernelType,
+    kernel: KernelType,
     dataset: FlatDataset,
-    cache:   Mat<f64>,
+    cache: Mat<f64>, 
 }
 
 impl FlatKernelCache {
     pub fn new(kernel: KernelType, dataset: FlatDataset, _size: usize) -> Self {
         let n = dataset.n_samples();
         let cache = Mat::<f64>::from_fn(n, n, |_, _| f64::NAN);
-        Self { kernel, dataset, cache }
+        FlatKernelCache { kernel, dataset, cache }
     }
 
-    #[inline]
     pub fn get(&mut self, i: usize, j: usize) -> f64 {
         let val = self.cache.read(i, j);
         if !val.is_nan() {
-            return val;                             
+            return val;
         }
-
-        let xi  = self.dataset.get_row(i);
-        let xj  = self.dataset.get_row(j);
+        let xi = self.dataset.get_row(i);
+        let xj = self.dataset.get_row(j);
         let val = self.kernel.compute_pair_flat(xi, xj);
-
         self.cache.write(i, j, val);
         self.cache.write(j, i, val);
         val
